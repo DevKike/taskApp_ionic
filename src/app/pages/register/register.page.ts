33f2fcad6321e7ccfe9ba47364664849d0bd98cb
sahir ruiz <sahirruiz4@gmail.com>
2024-09-29 16:46:54 -0500

    Refactor: implements interface on register form

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { IUser } from 'src/app/interfaces/IUser';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.page.html',
   styleUrls: ['./register.page.scss'],
 })
-export class RegisterPage {
-
+export class RegisterPage  {
   public email!: FormControl;
   public password!: FormControl;
+  public name!: FormControl;
+  public lastName!: FormControl;
+  public age!: FormControl;
+  public phoneNumber!: FormControl;
   public form!: FormGroup;
 
   constructor() {
@@ -18,9 +22,8 @@ export class RegisterPage {
 
   doRegister() {
     if (this.form.valid) {
-      const email = this.form.value.email;
-      const password = this.form.value.password;
-      console.log('Registro exitoso:', { email, password });
+      const user: IUser = this.form.value;
+      console.log('Registro exitoso:', user);
     } else {
       console.log('Formulario inválido');
     }
@@ -29,9 +32,18 @@ export class RegisterPage {
   private initForm() {
     this.email = new FormControl('', [Validators.email, Validators.required]);
     this.password = new FormControl('', [Validators.required]);
+    this.name = new FormControl('', [Validators.required]);
+    this.lastName = new FormControl('', [Validators.required]);
+    this.age = new FormControl('', [Validators.required, Validators.min(0)]);
+    this.phoneNumber = new FormControl('', [Validators.required]);
+
     this.form = new FormGroup({
       email: this.email,
       password: this.password,
+      name: this.name,
+      last_name: this.lastName,
+      age: this.age,
+      phone_number: this.phoneNumber,
     });
   }
 }
