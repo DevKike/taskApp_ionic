37e2e6d5ee1438ff4cb298013415f3b1d56096fd
DevKike <alvaronarvaez151@gmail.com>
2024-09-29 13:44:32 -0500

    Feat: add task and user interfaces

---
+++ src/app/interfaces/ITask.ts
@@ -0,0 +1,7 @@
+export interface ITask {
+  title: string;
+  description: string;
+  date: Date;
+  userId: number;
+  done: true;
+}
---
+++ src/app/interfaces/IUser.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+  id: number;
+  name: string;
+  last_name: string;
+  age: number;
+  phone_number: number;
+  email: string;
+  password: string;
+  image?: string;
+}
