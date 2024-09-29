6d17b9a7dd3a6fc4d4949ca2e0144b588fb50866
sahir ruiz <sahirruiz4@gmail.com>
2024-09-29 16:18:52 -0500

    Refactor: create register form and implements validators

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { NavController } from '@ionic/angular';
-import { AuthService } from 'src/app/modules/shared/auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -9,28 +7,22 @@ import { AuthService } from 'src/app/modules/shared/auth/auth.service';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage {
+
   public email!: FormControl;
   public password!: FormControl;
   public form!: FormGroup;
 
-  constructor(
-    private readonly authSrv: AuthService,
-    private readonly navCtrl: NavController
-  ) {
+  constructor() {
     this.initForm();
   }
 
-  public async doRegister() {
-    try {
-      console.log(this.form.value);
-      const response = await this.authSrv.register(
-        this.form.value.email,
-        this.form.value.password
-      );
-      console.log(response);
-     /*  this.navCtrl.navigateForward('/home'); */
-    } catch (error) {
-      console.error(error);
+  doRegister() {
+    if (this.form.valid) {
+      const email = this.form.value.email;
+      const password = this.form.value.password;
+      console.log('Registro exitoso:', { email, password });
+    } else {
+      console.log('Formulario inválido');
     }
   }
 
