4061960c1f98bd773a50b3ad9e1ba4dbd8246bd4
DevKike <alvaronarvaez151@gmail.com>
2024-09-29 13:44:59 -0500

    Feat: add core module and init firebase with credentials

---
+++ src/app/modules/core/firestore/core.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AngularFireModule } from "@angular/fire/compat";
+import { environment } from 'src/environments/environment.prod';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AngularFireModule.initializeApp(environment.FIREBASE_CREDENTIALS),
+  ]
+})
+export class CoreModule { }
