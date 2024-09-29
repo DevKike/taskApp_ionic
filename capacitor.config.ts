566f14aa3f897e2bfd52656a40ac77db96a10dfa
sahir ruiz <sahirruiz4@gmail.com>
2024-09-28 19:30:39 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'taskApp_FireBase',
+  webDir: 'www'
+};
+
+export default config;
