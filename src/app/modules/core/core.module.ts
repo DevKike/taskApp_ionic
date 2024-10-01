cb692376e400264140663fbb5fdd30195a2c6155
DevKike <alvaronarvaez151@gmail.com>
2024-10-01 01:05:08 -0500

    Feat: add ng firestorage module

--- src/app/modules/core/core.module.ts
+++ src/app/modules/core/core.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
-import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFireModule } from '@angular/fire/compat';
 import { environment } from 'src/environments/environment.prod';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 @NgModule({
   declarations: [],
   imports: [
     AngularFireModule.initializeApp(environment.FIREBASE_CREDENTIALS),
     AngularFireAuthModule,
-    AngularFirestoreModule
-  ]
+    AngularFirestoreModule,
+    AngularFireStorageModule
+  ],
 })
-export class CoreModule { }
+export class CoreModule {}
