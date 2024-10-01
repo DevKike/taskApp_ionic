1c87b6a4955f31937c111a5d4396bc3987d5f262
DevKike <alvaronarvaez151@gmail.com>
2024-10-01 01:05:44 -0500

    Feat: add storage service with upload method

--- src/app/modules/shared/services/storage/storage.service.ts
+++ src/app/modules/shared/services/storage/storage.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class StorageService {
+  constructor(private readonly _ngFireStorageSrv: AngularFireStorage) {}
 
-  constructor() { }
+  public async upload(filePath: string, file: any) {
+    const fileRef = this._ngFireStorageSrv.ref(filePath);
+    await this._ngFireStorageSrv.upload(filePath, file);
+  }
 }
