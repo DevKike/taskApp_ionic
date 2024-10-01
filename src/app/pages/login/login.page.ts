fc33216a00793fb65eb7453bc1c270278c9d2a90
DevKike <alvaronarvaez151@gmail.com>
2024-09-30 23:00:07 -0500

    Fix: update init form on constructor for correct operation

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -8,40 +8,37 @@ import { AuthService } from 'src/app/modules/shared/auth/auth.service';
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage{
-
+export class LoginPage {
   public email!: FormControl;
   public password!: FormControl;
   public form!: FormGroup;
 
-  constructor(private readonly navCtrl: NavController, private readonly authSrv: AuthService) 
-  { 
-    this.initForm;
+  constructor(private readonly navCtrl: NavController, private readonly authSrv: AuthService) {
+    this.initForm();
   }
 
   public async doLogin() {
-    if(this.form.value) {
-       const {email, password} = this.form.value;
-       try{
+    if (this.form.value) {
+      const { email, password } = this.form.value;
+      try {
         await this.authSrv.login(email, password);
         console.log('login successfully');
         this.navCtrl.navigateForward('/home');
-       } catch (error) { 
-        console.error('login failed', error)
-       }
+      } catch (error) {
+        console.error('login failed', error);
+      }
     } else {
-      console.warn('form not validated')
+      console.warn('form not validated');
     }
   }
 
-  private initForm(){
+  private initForm() {
     this.email = new FormControl('', [Validators.email, Validators.required]);
     this.password = new FormControl('', [Validators.required]);
 
     this.form = new FormGroup({
-      email : this.email,
-      password: this.password
-    })
+      email: this.email,
+      password: this.password,
+    });
   }
-
 }
