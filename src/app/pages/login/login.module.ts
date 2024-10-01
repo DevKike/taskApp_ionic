73331465efbbc245b73667697607ae59cbc4430a
DevKike <alvaronarvaez151@gmail.com>
2024-09-30 22:58:45 -0500

    Refactor: update imports

--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,8 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
 
 import { LoginPageRoutingModule } from './login-routing.module';
 
@@ -10,13 +6,7 @@ import { LoginPage } from './login.page';
 import { SharedModule } from 'src/app/modules/shared/shared.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    LoginPageRoutingModule,
-    SharedModule
-  ],
-  declarations: [LoginPage]
+  imports: [LoginPageRoutingModule, SharedModule],
+  declarations: [LoginPage],
 })
 export class LoginPageModule {}
--- src/app/pages/register/register.module.ts
+++ src/app/pages/register/register.module.ts
@@ -1,23 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
 
 import { RegisterPageRoutingModule } from './register-routing.module';
 
 import { RegisterPage } from './register.page';
 import { SharedModule } from 'src/app/modules/shared/shared.module';
 
-
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RegisterPageRoutingModule,
-    SharedModule
-  ],
-  declarations: [RegisterPage]
+  imports: [RegisterPageRoutingModule, SharedModule],
+  declarations: [RegisterPage],
 })
 export class RegisterPageModule {}
