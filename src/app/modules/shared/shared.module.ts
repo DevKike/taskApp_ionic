2a11dfa42d8cdf12f667084134548b0778f1f12f
DevKike <alvaronarvaez151@gmail.com>
2024-09-30 22:48:03 -0500

    Feat: add firebase service on providers

--- src/app/modules/shared/shared.module.ts
+++ src/app/modules/shared/shared.module.ts
@@ -6,27 +6,18 @@ import { IonicModule } from '@ionic/angular';
 import { InputComponent } from './components/input/input.component';
 import { ButtonComponent } from './components/button/button.component';
 import { CardComponent } from './components/card/card.component';
+import { FirebaseService } from './firebase/firebase.service';
 
-const MODULES = [
-  CommonModule,
-  FormsModule,
-  IonicModule,
-  ReactiveFormsModule,
-];
+const MODULES = [CommonModule, FormsModule, IonicModule, ReactiveFormsModule];
 
-const COMPONENTS = [
-  InputComponent,
-  ButtonComponent,
-  CardComponent
-];
+const COMPONENTS = [InputComponent, ButtonComponent, CardComponent];
 
+const PROVIDERS = [FirebaseService];
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [
-    ...MODULES
-  ],
-  providers: [],
-  exports: [...MODULES, ...COMPONENTS]
+  imports: [...MODULES],
+    providers: [...PROVIDERS],
+  exports: [...MODULES, ...COMPONENTS],
 })
 export class SharedModule {}
