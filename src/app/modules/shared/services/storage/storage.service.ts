5f7da2d2143c8c006d3c382a7491369a19240894
DevKike <alvaronarvaez151@gmail.com>
2024-10-02 15:23:12 -0500

    Feat: add get url from saved image method

--- src/app/modules/shared/services/storage/storage.service.ts
+++ src/app/modules/shared/services/storage/storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +9,21 @@ export class StorageService {
   constructor(private readonly _ngFireStorageSrv: AngularFireStorage) {}
 
   public async upload(filePath: string, file: any) {
-    const fileRef = this._ngFireStorageSrv.ref(filePath);
-    await this._ngFireStorageSrv.upload(filePath, file);
+    try {
+      await this._ngFireStorageSrv.upload(filePath, file);
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  public async getUrl(filePath: string) {
+    try {
+      const pathRef = this._ngFireStorageSrv.ref(filePath);
+      return await lastValueFrom(pathRef.getDownloadURL());
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   }
 }
