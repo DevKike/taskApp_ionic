7ab14beb83659244fa441cff808334606a5e5002
DevKike <alvaronarvaez151@gmail.com>
2024-10-02 23:05:36 -0500

    Chore: delete some unnecessary imports

--- src/app/pages/tasks/tasks.module.ts
+++ src/app/pages/tasks/tasks.module.ts
@@ -1,22 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-
-import { IonicModule } from '@ionic/angular';
 
 import { TasksPageRoutingModule } from './tasks-routing.module';
 
 import { TasksPage } from './tasks.page';
-import { SharedModule } from "../../modules/shared/shared.module";
+import { SharedModule } from '../../modules/shared/shared.module';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    TasksPageRoutingModule,
-    SharedModule
-],
-  declarations: [TasksPage]
+  imports: [TasksPageRoutingModule, SharedModule],
+  declarations: [TasksPage],
 })
 export class TasksPageModule {}
