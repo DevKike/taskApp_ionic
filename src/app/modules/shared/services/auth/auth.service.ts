cf94cb935bb38f44c557db5f8ba28255b66abb51
DevKike <alvaronarvaez151@gmail.com>
2024-10-06 13:36:20 -0500

    Chore: prettiering code

--- src/app/modules/shared/services/auth/auth.service.ts
+++ src/app/modules/shared/services/auth/auth.service.ts
@@ -1,33 +1,31 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from "@angular/fire/compat/auth";
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-
-  constructor(private readonly angularFire: AngularFireAuth) { }
+  constructor(private readonly _angularFire: AngularFireAuth) {}
 
   public async register(email: string, password: string) {
-    return await this.angularFire.createUserWithEmailAndPassword(email, password)
+    return await this._angularFire.createUserWithEmailAndPassword(email, password);
   }
 
   public async login(email: string, password: string) {
-    return await this.angularFire.signInWithEmailAndPassword(email, password);
+    return await this._angularFire.signInWithEmailAndPassword(email, password);
   }
 
   public async logout() {
-    return await this.angularFire.signOut();
+    return await this._angularFire.signOut();
   }
 
   public async isAuth() {
-    const user = await this.angularFire.currentUser;
+    const user = await this._angularFire.currentUser;
     return !!user;
   }
 
   public async getAuthUserId() {
-    const user = await this.angularFire.currentUser;
+    const user = await this._angularFire.currentUser;
     return user?.uid;
   }
-
 }
--- src/app/modules/shared/services/loading/loading.service.ts
+++ src/app/modules/shared/services/loading/loading.service.ts
@@ -2,27 +2,26 @@ import { Injectable } from '@angular/core';
 import { LoadingController } from '@ionic/angular';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoadingService {
-
   private loading: any;
-  constructor(private loadingCrl: LoadingController) { }
+  constructor(private loadingCrl: LoadingController) {}
 
-  async showLoading(message: string = "Cargando..."){
+  public async showLoading(message: string = 'Loading...') {
     this.loading = await this.loadingCrl.create({
       message,
-      spinner: "crescent",
-      cssClass: 'custom-loading'
+      spinner: 'crescent',
+      cssClass: 'custom-loading',
     });
 
     await this.loading.present();
   }
 
-  async hideLoading() {
+  public async hideLoading() {
     if (this.loading) {
       await this.loading.dismiss();
-      this.loading = null
+      this.loading = null;
     }
   }
 }
