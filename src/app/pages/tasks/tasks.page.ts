71d29524273c9823dadb12b783b1760f9531a380
Jared Trujillo <148577531+DevJaredt@users.noreply.github.com>
2024-10-07 00:50:46 -0500

    Feat: setting the task page

--- src/app/pages/tasks/tasks.page.ts
+++ src/app/pages/tasks/tasks.page.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/modules/shared/services/auth/auth.service';
+import { FirestoreService } from 'src/app/modules/shared/services/firestore/firestore.service';
 
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.page.html',
   styleUrls: ['./tasks.page.scss'],
 })
-export class TasksPage {
-  
+export class TasksPage implements OnInit {
+  filteredTasks: any[] = [];
+
+  constructor(
+    private readonly _firestoreSrv: FirestoreService,
+    private readonly _authSrv: AuthService  
+  ) {}
+
+  async ngOnInit() {
+    this.loadTasks();
+  }
+
+  protected async loadTasks() {
+    try {
+      const currentUserId = await this._authSrv.getAuthUserId();
+      const tasks = await this._firestoreSrv.getCollections('tasks');
+      console.log('Todas las tareas recuperadas:', tasks); 
+      this.filteredTasks = tasks.filter(task => task.userId === currentUserId).map(task => ({
+        ...task,
+        creationDate: task.creationDate || new Date()
+      }))
+      console.log('Tareas filtradas para el usuario actual:', this.filteredTasks);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  protected async deleteTasks(taskId: string) {
+    try {
+      await this._firestoreSrv.delete('task', taskId);
+      console.log('Eliminando tarea con ID:', taskId); 
+      this.filteredTasks = this.filteredTasks.filter(task => taskId !== taskId)
+      console.log('Tareas filtradas actualizadas:', this.filteredTasks);
+      await this.loadTasks();
+    } catch {}
+  }
 }
