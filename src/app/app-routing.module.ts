4553682182c30ae6f1998ef3d266d0b35120c0f8
DevKike <alvaronarvaez151@gmail.com>
2024-10-07 15:01:54 -0500

    Fix: update import

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './modules/shared/services/auth.guard';
+import { AuthGuard } from './modules/shared/services/guard/auth.guard';
 
 const routes: Routes = [
   {
