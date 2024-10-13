d95b6ecd636f232ff130701c72a029666037e23b
DevKike <alvaronarvaez151@gmail.com>
2024-10-12 19:40:12 -0500

    Fix: update load and delete tasks logic

--- src/app/pages/tasks/tasks.page.ts
+++ src/app/pages/tasks/tasks.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ITask } from 'src/app/interfaces/ITask';
 import { AuthService } from 'src/app/modules/shared/services/auth/auth.service';
 import { FirestoreService } from 'src/app/modules/shared/services/firestore/firestore.service';
 import { LoadingService } from 'src/app/modules/shared/services/loading/loading.service';
@@ -9,8 +10,8 @@ import { ToastService } from 'src/app/modules/shared/services/toast/toast.servic
   templateUrl: './tasks.page.html',
   styleUrls: ['./tasks.page.scss'],
 })
-export class TasksPage implements OnInit {
-  filteredTasks: any[] = [];
+export class TasksPage {
+  protected tasks!: ITask[];
 
   constructor(
     private readonly _firestoreSrv: FirestoreService,
@@ -19,32 +20,28 @@ export class TasksPage implements OnInit {
     private readonly _toastSrv: ToastService
   ) {}
 
-  ngOnInit() {
+  ionViewWillEnter() {
     this.loadTasks();
   }
 
   protected async loadTasks() {
     try {
-      const currentUserId = await this._authSrv.getAuthUserId();
+      const userId = await this._authSrv.getAuthUserId();
       const tasks = await this._firestoreSrv.getDocumentsByCollection('task');
-      this.filteredTasks = tasks
-        .filter((task) => task.userId === currentUserId)
-        .map((task) => ({ ...task, creationDate: task.creationDate.toDate() }));
-      this.filteredTasks = [...this.filteredTasks];
+      const userTasks = tasks.filter((task) => task.userId === userId);
+      this.tasks = userTasks;
     } catch (error) {
       throw error;
     }
   }
 
-
-     async deleteTasks(taskId: string) {
+  protected async deleteTasks(taskId: string) {
     try {
       this._loadingSrv.showLoading('Deleting Task...');
       await this._firestoreSrv.delete('task', taskId);
-      this.filteredTasks = this.filteredTasks.filter((task) => task.id !== taskId);
       this._loadingSrv.hideLoading();
       this._toastSrv.presentToast('Task deleted successfully');
-      await this.loadTasks(); 
+      await this.loadTasks();
     } catch (error) {
       this._loadingSrv.hideLoading();
       this._toastSrv.presentErrorToast('Error deleting task');
