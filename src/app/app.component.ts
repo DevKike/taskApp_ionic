41c4c2efc3f6380b2b85a13f4fbd1b95def71047
DevKike <alvaronarvaez151@gmail.com>
2024-10-14 18:03:03 -0500

    Fix: update menu for only show when user is auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+import { AuthService } from './modules/shared/services/auth/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -10,16 +11,27 @@ import { MenuController } from '@ionic/angular';
 export class AppComponent {
   protected showMenu: boolean = false;
 
-  constructor(private readonly _router: Router, private readonly _menu: MenuController) {
+  constructor(
+    private readonly _router: Router,
+    private readonly _menu: MenuController,
+    private readonly _authService: AuthService
+  ) {
     this._router.events.subscribe((event) => {
       if (event instanceof NavigationEnd) {
-        this.showMenu =
-          event.url === '/principal' ||
-          event.url === '/task' ||
-          event.url === '/tasks' ||
-          event.url === '/configuration';
-        this._menu.close();
+        this.checkIdAndSetMenu(event.url);
       }
     });
+  }
+
+  private async checkIdAndSetMenu(url: string) {
+    const userId = await this._authService.getAuthUserId();
+    if (userId !== 0) {
+      this.showMenu =
+        url === '/principal' ||
+        url === '/task' ||
+        url === '/tasks' ||
+        url === `/register/${userId}`;
+      this._menu.close();
+    }
   }
 }
